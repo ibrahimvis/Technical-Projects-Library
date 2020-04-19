7e2406401a580c872ab78856d902527027efb98c
GhadeerAbdulaziz <ghadeeraziz12@gmail.com>
2020-04-19 21:12:13 +0300

    fixed login

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,35 @@ import CreateProject from "./component/project/CreateProject";
 import Signup from "./component/user/Signup";
 import ChangePassword from "./component/user/ChangePassword";
 import Nave from "./component/navbar/Nave";
-import {Login} from "./component/user/Login";
-import AllProjects from './component/project/AllProjects';
+import { Login } from "./component/user/Login";
+import AllProjects from "./component/project/AllProjects";
 import OneProject from "./component/project/OneProject";
-
+import jwt_decode from "jwt-decode";
 export default class App extends Component {
   state = {
-    isAuth: false,
-    user: {_id: "5e9c1cd7bc5da5114f224aab"}, // temp change it to null
-    message: null,
+    user: null,
+    isLogin: false,
   };
 
-  logoutHandler = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token");
-    this.setState({
-      isAuth: false,
-      user: null,
-      message: null,
-    });
-  };
+  componentDidMount() {
+    this.userLogin();
+  }
 
+  userLogin = () => {
+    if (localStorage.token) {
+      let token = localStorage.token;
+      let user = jwt_decode(token, "SECRET").user;
+      this.setState({
+        user: user,
+        isLogin: true,
+      });
+    } else {
+      this.setState({
+        user: null,
+        isLogin: false,
+      });
+    }
+  };
   render() {
     const { isAuth, message, user } = this.state;
     return (
@@ -45,14 +53,16 @@ export default class App extends Component {
           /> */}
           <Route path="/allproject" component={AllProjects} />
           <Route path="/signup" component={Signup} />} />
-
-          <Route path= '/login' render ={ (props) => <Login  {...props} userLogin = {this.userLogin}/>} />
-
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} userLogin={this.userLogin} />}
+          />
           <Route
             path="/changepassword"
-            render={(props) => <ChangePassword {...props} user={this.state.user} />}
+            render={(props) => (
+              <ChangePassword {...props} user={this.state.user} />
+            )}
           />
-
         </Switch>
       </div>
     );
